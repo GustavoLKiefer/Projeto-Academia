ab10e1cc58e11fde3dece2cf72b4340a29719a81
Gustavo <165567212+GustavoLKiefer@users.noreply.github.com>
2025-06-12 20:48:26 -0300

    Eu odeio ter que commitar assim pra não explodir o código

--- ProjetoAcademia/code/demo/src/main/java/model/Treino.java
+++ ProjetoAcademia/code/demo/src/main/java/model/Treino.java
@@ -10,50 +10,39 @@ public class Treino {
     private int duracao_minutos;
     private Date data_inicio;
 
-    public int getId() {
+    public int getIdTreino() {
         return id;
     }
-
-    public void setId(int id) {
+    public void setIdTreino(int id) {
         this.id = id;
     }
-
     public int getIdAluno() {
         return aluno_id;
     }
-
     public void setIdAluno(int idAluno) {
         this.aluno_id = idAluno;
     }
-
     public String getTipoTreino() {
         return tipo_treino;
     }
-
     public void setTipoTreino(String tipoTreino) {
         this.tipo_treino = tipoTreino;
     }
-
     public String getDescricao() {
         return descricao;
     }
-
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
-
     public int getDuracaoMinutos() {
         return duracao_minutos;
     }
-
     public void setDuracaoMinutos(int duracaoMinutos) {
         this.duracao_minutos = duracaoMinutos;
     }
-
     public Date getDataInicio() {
         return data_inicio;
     }
-
     public void setDataInicio(Date dataInicio) {
         this.data_inicio = dataInicio;
     }
